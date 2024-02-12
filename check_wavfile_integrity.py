b26285b55f933b347bcd05bae5bce207f383bbac
Mark Simmons <marks21@utexas.edu>
2024-02-12 12:20:12 -0800

    wrote script to open all wavs in dir w scipy

---
+++ check_wavfile_integrity.py
@@ -0,0 +1,26 @@
+from argparse import ArgumentParser
+from typing import Sequence, Optional
+from scipy.io import wavfile
+from glob import glob
+
+
+def main(argv: Optional[Sequence[str]] = None):
+    parser = ArgumentParser('Check wavfile integrity')
+    parser.add_argument('WAVDIR')
+    parser.add_argument('--recursive', '-r', action='store_true')
+
+    args = parser.parse_args(argv)
+
+    wav_dir = args.WAVDIR
+    if args.recursive:
+        wav_fps = glob(wav_dir+'**/*.wav', recursive=True)
+    else:
+        wav_fps = glob(wav_dir+'*.wav')
+    [check_wav_for_warning(wav_fp) for wav_fp in wav_fps]
+    
+def check_wav_for_warning(wav_fp: str):
+    wavfile.read(wav_fp) # for now just let warning get printed to stdout
+
+
+if __name__ == '__main__':
+    main()
