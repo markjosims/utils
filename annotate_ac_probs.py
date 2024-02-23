7e8c68a7510d2841488b85adfe02b9c4bb7b7c65
Mark Simmons <marks21@utexas.edu>
2024-02-23 15:06:06 -0800

    add tgt_lang arg

--- annotate_ac_probs.py
+++ annotate_ac_probs.py
@@ -14,6 +14,7 @@ def main(argv: Optional[Sequence[str]] = None):
     parser.add_argument('OUTDIR')
     parser.add_argument('--recursive', '-r', action='store_true')
     parser.add_argument('--tier', '-t')
+    parser.add_argument('--tgt_lang')
 
     args = parser.parse_args(argv)
 
@@ -44,6 +45,7 @@ def main(argv: Optional[Sequence[str]] = None):
             task='LID',
             max_len=20,
             tier=args.tier,
+            tgt_lang=args.tgt_lang,
         )
         eaf_stem = os.path.basename(eaf_fp)
         out_fp = os.path.join(out_dir, eaf_stem)
